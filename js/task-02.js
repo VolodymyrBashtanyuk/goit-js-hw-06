86648c3253fceca5c3c62d5d02e5b91aede05759
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-06-24 12:35:30 +0300

    task-2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const isIngredients = document.querySelector('#ingredients');
+
+const isNameIngredients = ingredients.map(gradient  => {
+  const isNewIngredient = document.createElement('li');
+  isNewIngredient.classList.add('js-item');
+  isNewIngredient.textContent = gradient;
+  return isNewIngredient;
+});
+ isIngredients.append(...isNameIngredients);
