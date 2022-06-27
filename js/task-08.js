dd8fa45f2668e3c9926630ae1f8ca862b5acd05b
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-06-27 17:28:55 +0300

    task-08 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const getForm = document.querySelector('.login-form');
+
+const getInput = document.querySelectorAll('input')
+getInput[0].removeAttribute('type');
+
+
+getForm.addEventListener('submit', onFormSubmit)
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    if (getInput[0].value=== ' ' || !getInput[0].value.includes('@') ) {
+        alert('Enter valid Email')
+    } else if (getInput[1].value === '') {
+        alert('Enter password')
+    } else {
+        const formData = new FormData(event.currentTarget);
+    
+        formData.forEach((name, value) => {
+        console.log('name:',value);
+        console.log('value:', name);
+        
+        getForm.reset();
+        
+    });
+    };   
+
+
+    
+    
+
+};
 
