3d8a89a1e406026c132edbfac359d8f3a866c8c9
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-06-30 07:52:52 +0300

    task-09 correction done

--- js/task-09.js
+++ js/task-09.js
@@ -8,9 +8,19 @@ getButtonChangeColor.addEventListener('click', onChangeColor);
 
 function onChangeColor() {
   getBackgroundColor.style.backgroundColor = getRandomHexColor();
-  getColor.textContent = getRandomHexColor(); 
+  getColor.textContent = rgbhex(getBackgroundColor.style.backgroundColor); //hex
+  // getColor.textContent = getBackgroundColor.style.backgroundColor; // rgb
 };
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function rgbhex(rgb) {
+  let RGB = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  
+  return (RGB && RGB.length === 4) ? "#" +
+    ("0" + parseInt(RGB[1], 10).toString(16)).slice(-2) +
+    ("0" + parseInt(RGB[2], 10).toString(16)).slice(-2) +
+    ("0" + parseInt(RGB[3], 10).toString(16)).slice(-2) : '';
 };
