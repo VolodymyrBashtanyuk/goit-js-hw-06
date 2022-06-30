978c01c3e9d45fe5b6fe0f2b39d3986e48604057
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-06-30 07:11:07 +0300

    task-05 correction done

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,13 @@ const nameInput = document.querySelector('#name-input');
 const titleUserName = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', onInputChange);
+console.log(titleUserName.textContent)
 
-
-function onInputChange(event) {
-    titleUserName.textContent = event.currentTarget.value;
+function onInputChange(event) { 
+   if (event.currentTarget.value === '') {
+        titleUserName.textContent = 'Anonymous';
+   } else {
+       titleUserName.textContent = event.currentTarget.value;
+    };
+       
 };
